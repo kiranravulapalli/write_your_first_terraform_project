d83cd431983a5e4c05a24ce797fe51b2e7f3f0ef
kiranravulapalli <kirankumarpalli132@gmail.com>
2024-07-17 20:42:33 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -10,15 +10,14 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0ad21ae1d0696ad58"
   instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
-
